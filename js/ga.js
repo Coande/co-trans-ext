af5bf0bc459bc3098056c7d7ab51aeb5d8c31d56
Coande <e12e@qq.com>
2019-04-22 00:37:31 +0800

    添加 扩展版本号 到 GA

--- js/ga.js
+++ js/ga.js
@@ -11,7 +11,10 @@ ga('create', propertyID, 'auto');
 // 以下必须设置，否则会检查协议，默认只支持http和https
 ga('set', 'checkProtocolTask', null);
 
+// 只要设置 appVersion，Google Analytics 就接收不到数据，是一个bug。只要自定义一个维度。
+// https://stackoverflow.com/questions/36508241/how-do-i-set-appversion-for-google-analytics-event-tracking
 // ga('set', 'appVersion', chrome.runtime.getManifest().version);
+ga('set', 'dimension1', chrome.runtime.getManifest().version);
 
 // 设置是否停用
 function setIsDisableGA(isDisable) {
